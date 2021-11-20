5e3c953cd5a832d52a70fade31d976c0146140fb
vechkitov <vechkitov@bk.ru>
2021-11-20 15:55:24 +0200

    2. Каркас шахматной доски [#792 #230202]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -32,7 +32,7 @@ public final class Logic {
         for (Figure figure : figures) {
             if (figure != null) {
                 for (Cell step : steps) {
-                    if (step == figure.position()) {
+                    if (figure.position().equals(step)) {
                         throw new OccupiedCellException();
                     }
                 }
